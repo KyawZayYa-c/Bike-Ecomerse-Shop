f0e5e70366e6539cc98278dc1fcc75149a337034
Kyaw Zay Ya <zayya4281@gmail.com>
2026-03-25 10:31:11 +0630

    Fix port to 8080 next fixed

--- server/server.js
+++ server/server.js
@@ -21,61 +21,6 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-    origin : process.env.CLIENT_URL,
-    methods : ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders : [
-        "Content-type",
-        "Authorization",
-        "Cache-Control",
-        "Expires",
-        "Pragma"
-    ],
-    credentials : true
-}))
-
-app.use(cookieParser());
-app.use(express.json());
-
-app.use("/api/auth", authRouter)
-app.use("/api/admin/products", adminProductsRouter);
-app.use('/api/admin/orders', adminOrderRouter)
-
-app.use("/api/shop/products", shopProductsRouter);
-app.use("/api/shop/cart", shopCartRouter);
-app.use("/api/shop/address", shopAddressRouter);
-app.use("/api/shop/order", shopOrderRouter);
-app.use('/api/shop/search', shopSearchRouter);
-app.use('/api/shop/reviews', shopReviewRouter);
-
-app.use('/api/common/feature', commonFeaturesRouter);
-
-app.listen(PORT , () => console.log("Server is running on port "+ PORT));
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-const cors = require('cors');
-const authRouter = require('./routes/auth/auth-routes');
-const adminProductsRouter = require('./routes/admin/products-routes');
-const adminOrderRouter = require('./routes/admin/order-routes');
-
-const shopProductsRouter = require('./routes/shop/products-routes');
-const shopCartRouter = require('./routes/shop/cart-routes');
-const shopAddressRouter = require('./routes/shop/address-routes');
-const shopOrderRouter = require('./routes/shop/order-routes');
-const shopSearchRouter = require('./routes/shop/search-routes');
-const shopReviewRouter = require('./routes/shop/review-routes');
-
-const commonFeaturesRouter = require('./routes/common/feature-routes');
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Connected to MongoDb'))
-    .catch(err => console.log(err));
-
-const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
