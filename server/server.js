78e264f0f277459fba3955020bdd41cb17454c42
Kyaw Zay Ya <zayya4281@gmail.com>
2026-03-25 10:37:01 +0630

    Fix port to 8080 fixed

--- server/server.js
+++ server/server.js
@@ -1,24 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+
 const authRouter = require('./routes/auth/auth-routes');
 const adminProductsRouter = require('./routes/admin/products-routes');
 const adminOrderRouter = require('./routes/admin/order-routes');
-
 const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter = require('./routes/shop/cart-routes');
 const shopAddressRouter = require('./routes/shop/address-routes');
 const shopOrderRouter = require('./routes/shop/order-routes');
 const shopSearchRouter = require('./routes/shop/search-routes');
 const shopReviewRouter = require('./routes/shop/review-routes');
-
 const commonFeaturesRouter = require('./routes/common/feature-routes');
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Connected to MongoDb'))
-    .catch(err => console.log(err));
+    .then(() => console.log('Connected to MongoDb'))
+    .catch(err => console.log('MongoDB connection error:', err));
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -34,22 +33,21 @@ app.use(cors({
         "Pragma"
     ],
     credentials : true
-}))
+}));
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth", authRouter)
+// API Routes များ
+app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
-app.use('/api/admin/orders', adminOrderRouter)
-
+app.use('/api/admin/orders', adminOrderRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use('/api/shop/search', shopSearchRouter);
 app.use('/api/shop/reviews', shopReviewRouter);
-
 app.use('/api/common/feature', commonFeaturesRouter);
 
-app.listen(PORT , () => console.log("Server is running on port "+ PORT));
+app.listen(PORT , () => console.log("Server is running on port " + PORT));
